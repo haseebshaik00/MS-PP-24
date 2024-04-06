b68d7b0098731d13ad76e66649a36526057766af
haseebshaik00 <haseebshaik00@gmail.com>
2024-04-07 00:07:22 +0530

    Binary Search done

---
+++ DSA/Searching/binarySearch.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int bSearch(int a[], int n, int key){
+    int i=0, j=n-1, mid;
+    while(i<=j){
+        mid = (i+j)/2;
+        if(a[mid] == key) return mid;
+        else if(a[mid] < key) i=mid+1;
+        else j=mid-1;
+    }
+    return 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, key;
+    cin>>n;
+    int a[n];
+    memset(a, 0, n);
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+    cin>>key;
+    cout<<"Position = "<<bSearch(a, n, key)<<endl;
+
+    return 0;
+}
