ba479fbeede1d952933c29de17f394f9775f9f3a
haseebshaik00 <haseebshaik00@gmail.com>
2024-06-25 21:54:44 +0530

    revised 3.1

--- DSA/Codes/0-GeneralConcepts/rough.cpp
+++ DSA/Codes/0-GeneralConcepts/rough.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    int a[n];
+    memset(a, n, 0);
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+    for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+            for(int k=i; k<=j; k++)
+                cout<<a[k]<<" ";
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
