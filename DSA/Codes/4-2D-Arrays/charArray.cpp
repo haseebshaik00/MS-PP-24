83fd829c753a0a13a62dbc7d5da90785bbc09ea0
haseebshaik00 <haseebshaik00@gmail.com>
2024-06-29 04:32:10 +0530

    done with 2d arrays

---
+++ DSA/Codes/4-2D-Arrays/charArray.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char c[][12] = {{'h','i','\0'}, {'y','o','\0'}};
+    char d[10] = "haseeb";
+    cout<<c[0]<<" "<<d;
+
+    return 0;
+}
