4eaeb2886852b741e44026dc9416c10849819046
haseebshaik00 <haseebshaik00@gmail.com>
2024-07-04 21:09:57 +0530

    revised staircase search

--- DSA/Codes/0-GeneralConcepts/rough.cpp
+++ DSA/Codes/0-GeneralConcepts/rough.cpp
@@ -5,10 +5,24 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    char d[5] = "zoom";
-    char c[] = "haseeb";
-    cout<<sizeof(c)<<" "<<strlen(c)<<endl; // 7
-    cout<<c + 3;
+    int m, n, k, s=0;
+    cin>>m>>n;
+    int a[m][n];
+    for(int i=0; i<m; i++)
+        for(int j=0; j<n; j++)
+            cin>>a[i][j];
+    int i=0, j=n-1;
+    cin>>k;
+    while(i<m && j>=0){
+        s = a[i][j];
+        if(s == a[i][j]){
+            cout<<"Present"<<endl;
+            return 0;
+        }
+        else if(s < a[i][j]) j--;
+        else i++;
+    }
+    cout<<"Absent"<<endl;
 
     return 0;
 }
