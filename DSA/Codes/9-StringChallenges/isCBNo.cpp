016564e09047aa78326ebb0e48edd0f63dc0eaac
haseebshaik00 <haseebshaik00@gmail.com>
2024-07-11 04:02:22 +0530

    done with string challenges

--- DSA/Codes/9-StringChallenges/isCBNo.cpp
+++ DSA/Codes/9-StringChallenges/isCBNo.cpp
@@ -1,88 +1,54 @@
-#include <iostream>
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-long stoint(string s)
-{
-    long i, j = 0;
-    i = 0;
-    while (s[j] >= '0' && s[j] <= '9')
-    {
-        i = i * 10 + (s[j] - '0');
-        j++;
-    }
-    return i;
+bool checkCB(string subStr){
+	long long a = stoll(subStr);
+	if(a==0 || a==1)
+		return false;
+	int b[10]= {2,3,5,7,11,13,17,19,23,29};
+	int l = sizeof(b)/sizeof(b[0]);
+	for(int i=0; i<l; i++){
+		if(a == b[i])
+			return true;
+	}
+	for(int i=0; i<l; i++){
+		if(a % b[i] == 0)
+			return false;
+	}
+	return true;
 }
 
-bool is_cb_number(string s)
-{
-    long long num = stoint(s);
-    if (num == 0 || num == 1)
-    {
-        return false;
-    }
-    int arr[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-
-    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
-    {
-        if (num == arr[i])
-        {
-            return true;
-        }
-    }
-
-    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
-    {
-        if (num % arr[i] == 0)
-        {
-            return false;
-        }
-    }
-
-    return true;
+bool isValid(bool *visited, int i, int j){
+	for(int k=i; k<j; k++){
+	if(visited[k]) return false;
+	}
+	return true;
 }
 
-bool is_valid(bool *visited, int l, int r)
-{
-    for (int i = l; i < r; i++)
-    {
-        if (visited[i])
-        {
-            return false;
-        }
-    }
-    return true;
-}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-int main()
-{
-    int n;
-    cin >> n;
+	int n;
+    cin>>n;
     string s;
-    cin >> s;
-    long count = 0;
-    bool *visited = new bool[n];
-    for (int i = 0; i < s.length(); i++)
-    {
-        visited[i] = false;
-    }
-    for (int i = 1; i <= s.length(); i++)
-    {
-        for (int j = 0; j <= s.length() - i; j++)
-        {
-            int e = j + i;
-            string sub = s.substr(j, e - j);
-            if (is_cb_number(sub) && is_valid(visited, j, e))
-            {
-                count++;
-                for (int k = j; k < e; k++)
-                {
-                    visited[k] = true;
-                }
-            }
-        }
-    }
-    cout << count << endl;
-    return 0;
+    cin>>s;
+    long c = 0;
+	bool* visited = new bool[n];
+	for(int i=0; i<s.length(); i++)
+		visited[i] = false;
+	for(int i=1; i<=s.length(); i++){
+		for(int j=0; j<=s.length()-i; j++){
+			int e = j+i;
+			string subStr = s.substr(j, e-j);
+			if(checkCB(subStr) && isValid(visited, j, e)){
+				c++;
+				for(int k=j; k<e; k++)
+					visited[k] = true;
+			}
+		}
+	}
+	cout<<c<<endl;
+
+	return 0;
 }
