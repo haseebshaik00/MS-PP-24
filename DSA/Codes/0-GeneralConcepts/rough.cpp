1be406bef28bc97db2faab378ddedcb1f8282ff9
haseebshaik00 <haseebshaik00@gmail.com>
2024-07-12 23:55:28 +0530

    Added pointers

--- DSA/Codes/0-GeneralConcepts/rough.cpp
+++ DSA/Codes/0-GeneralConcepts/rough.cpp
@@ -5,12 +5,16 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	char a[] = "geeks";
-	char *p = "geeks";
-    a[1] = 'a';
-	cout<<a<<" "<<*p<<" "<<sizeof(*p);
-
-	// int *c = 2; -> not allowed
-
+	string s ="haseeb";
+	int a[]= {1,2,3};
+	for(char it: s)
+        cout<<it<<" ";
+    cout<<endl;
+    int *it = find(a, a+3, 2);
+    auto it1 = find(a, a+3, 2);
+    cout<<*it<<endl;
+    cout<<it1<<" : "<<endl;
+    for(auto it=s.begin(); it!=s.end(); it++)
+        cout<<*it<<" - ";
 	return 0;
 }
