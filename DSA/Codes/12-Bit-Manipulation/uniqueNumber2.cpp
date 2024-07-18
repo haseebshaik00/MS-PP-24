93f4d2ffec253cb6e74f44f5591cc8794f3d4e73
haseebshaik00 <haseebshaik00@gmail.com>
2024-07-18 21:13:15 +0530

    done with 13.6

--- DSA/Codes/12-Bit-Manipulation/uniqueNumber2.cpp
+++ DSA/Codes/12-Bit-Manipulation/uniqueNumber2.cpp
@@ -1,32 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int getBit(int n, int i){
-    int a = 1<<i;
-    return n&a>0 ? 1:0;
-}
-
 int main(){
     ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    int n, totalXor, pos=0, d=0, totalXorCopy;
-    cin>>n;
-    int a[n];
-    memset(a, n, 0);
-    for(int i=0; i<n; i++)
-        cin>>a[i];
-    for(int i=0; i<n; i++)
-        totalXor ^= a[i];
-    totalXorCopy = totalXor;
-    while(d==0){
-        if(totalXorCopy&1)
-            d++;
-        totalXorCopy = totalXorCopy>>1;
-    }
-    for(int i=0; i<n; i++){
-        if(getBit(a[i], d))
-            pos ^= a[i];
-    }
-    cout<<pos;
+    int n, bitXor=0, pos=-1, subAns=0, bitXorCopy=0, subAns1=0;
+	cin>>n;
+	int a[n];
+	for(int i=0; i<n; i++)
+		cin>>a[i];
+	for(int i=0; i<n; i++)
+		bitXor ^= a[i];
+	bitXorCopy = bitXor;
+	while(pos==-1){
+		if(bitXorCopy & 1)
+			pos++;
+		bitXorCopy>>1;
+	}
+	for(int i=0; i<n; i++){
+		if(a[i] & (1<<pos))
+			subAns ^= a[i];
+	}
+	subAns1 = subAns ^ bitXor;
+	cout<<min(subAns, subAns1)<<" "<<max(subAns, subAns1);
     return 0;
 }
