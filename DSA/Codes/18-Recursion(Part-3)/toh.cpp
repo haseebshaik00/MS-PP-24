c72b09342459d828e4dc6c6491aec81a710e414f
haseebshaik00 <haseebshaik00@gmail.com>
2024-08-02 05:55:27 +0530

    Added Tower of Hanoi

--- DSA/Codes/18-Recursion(Part-3)/toh.cpp
+++ DSA/Codes/18-Recursion(Part-3)/toh.cpp
@@ -1,11 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int countIt = 0;
+
+void toh(int n, char src, char helper, char dest){
+    if(n==0)
+        return;
+    toh(n-1, src, dest, helper);
+    cout<<"Move "<<n<<" from "<<src<<" to "<<dest<<endl;
+    countIt++;
+    toh(n-1, helper, src, dest);
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-
+    int n;
+    cin>>n;
+    toh(n, 'A', 'B', 'C');
+    cout<<"Total Steps = "<<countIt<<endl;
 
     return 0;
 }
