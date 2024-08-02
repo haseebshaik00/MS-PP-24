de26d236900deb6457720989e143728fb58f2929
haseebshaik00 <haseebshaik00@gmail.com>
2024-08-02 05:34:05 +0530

    Added Friends Problem and Optimal Binary String Problem

--- DSA/Codes/18-Recursion(Part-3)/friendsProblem.cpp
+++ DSA/Codes/18-Recursion(Part-3)/friendsProblem.cpp
@@ -1,11 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int friends(int n){
+    if(n==0 || n==1) return 1;
+    if(n<0) return 0;
+    return friends(n-1)+(n-1)*friends(n-2);
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-
+    int n;
+    cin>>n;
+    cout<<friends(n);
 
     return 0;
 }
--- DSA/Codes/18-Recursion(Part-3)/optimalBinaryString.cpp
+++ DSA/Codes/18-Recursion(Part-3)/optimalBinaryString.cpp
@@ -1,11 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int binaryString(int n){
+    if(n==0 || n==1)
+        return n+1;
+    return binaryString(n-1) + binaryString(n-2);
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-
+    int n;
+    cin>>n;
+    cout<<binaryString(n)<<endl;
 
     return 0;
 }
