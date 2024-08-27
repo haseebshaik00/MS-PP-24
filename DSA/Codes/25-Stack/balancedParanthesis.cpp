ba24e16fadb54b853b94a0ee64ddf440d3c1065a
haseebshaik00 <haseebshaik00@gmail.com>
2024-08-28 04:43:22 +0530

    Added Stack Balanced Parathesis

---
+++ DSA/Codes/25-Stack/balancedParanthesis.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            char c = s[i];
+            if(st.empty())
+                st.push(c);
+            else if(c=='(' || c=='{' || c=='[')
+                st.push(c);
+            else if((c==')' && st.top()=='(') || (c=='}' && st.top()=='{') ||
+                    (c==']' && st.top()=='['))
+                st.pop();
+            else return false;
+        }
+        return st.empty();
+    }
+};
