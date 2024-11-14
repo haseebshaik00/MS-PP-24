3607227cccbabfb2c07b4e26025f26c4fbd6fd92
haseebshaik00 <haseebshaik00@gmail.com>
2024-11-14 02:15:48 -0800

    Add Graphs 38.13

--- DSA/Codes/38-Graphs/directedCycleDFS.cpp
+++ DSA/Codes/38-Graphs/directedCycleDFS.cpp
@@ -11,7 +11,28 @@ public:
             l[y].push_back(x);
     }
 
+    bool detectCycleHelper(int node, vector<bool> &visited, vector<bool> &stack){
+        visited[node] = true;
+        stack[node] = true;
 
+        for(auto x: l[node]){
+            if(stack[x])
+                return true;
+            else if(!visited[x]){
+                bool hasCycle = detectCycleHelper(x, visited, stack);
+                if(hasCycle) return true;
+            }
+        }
+        stack[node] = false;
+        return false;
+    }
+
+    bool detectCycle(){
+        int n = l.size();
+        vector<bool> visited(n, false);
+        vector<bool> stack(n, false);
+        return detectCycleHelper(0, visited, stack);
+    }
 };
 
 int main(){
@@ -20,10 +41,13 @@ int main(){
 
     Graph g;
     g.addEdge(0, 1);
-    g.addEdge(, 2);
+    g.addEdge(1, 2);
     g.addEdge(2, 3);
-    //g.addEdge(1, 2);
     g.addEdge(3, 4);
+    g.addEdge(4, 5);
+    g.addEdge(1, 5);
+    g.addEdge(5, 6);
+    // g.addEdge(4, 2);
 
     if(g.detectCycle()) cout<<"Cycle exists!";
     else cout<<"Cycle doesn't exists!";
