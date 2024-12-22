477315a120897249349c41c13871e100606830ed
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2024-12-22 03:43:48 -0800

    Revised I and II

--- DSA/Codes/0-GeneralConcepts/rough.cpp
+++ DSA/Codes/0-GeneralConcepts/rough.cpp
@@ -1,70 +1,31 @@
 #include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-class BST{
-public:
-	int data;
-	BST* left;
-	BST* right;
-
-	BST(int d){
-		data = d;
-		left = NULL;
-		right = NULL;
-	}
-};
-
-BST* insertNode(BST* root, int d){
-	if(root == NULL)
-		return new BST(d);
-	if(root->data < d)
-		root->right = insertNode(root->right, d);
-	else
-		root->left = insertNode(root->left, d);
-	return root;
+bool comp(int a, int b){
+	return a>b;
 }
 
-BST* build(){
-	int n, d;
-	cin>>n;
-	BST *root = NULL;
-	while(n--){
-		cin>>d;
-		root = insertNode(root, d);
-	}
-	return root;
-}
-
-void preOrderPrint(BST* root){
-	if(root == NULL)
-		return;
-	cout<<root->data<<" ";
-	preOrderPrint(root->left);
-	preOrderPrint(root->right);
-}
-
-void rangePrint(BST* root, int a, int b){
-	if(root == NULL)
-		return;
-	if(root->data >= a && root->data<=b)
-		cout<<root->data<<" ";
-
+void bubbleSort(int a[], int n, bool &cmp(int a, int b)){
+	for(int i=0; i<n-1; i++)
+		for(int j=0; j<n-1-i; j++)
+			if(cmp(a[j], a[j+1]))
+				swap(a[j], a[j+1]);
 }
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int t;
-	cin>>t;
-	while(t--){
-		BST* root = build();
-		preOrderPrint(root);
-		int a, b;
-		cin>>a>>b;
-		rangePrint(root, a, b);
-		cout<<endl;
-	}
+	int n;
+	cin>>n;
+	int a[n];
+	memset(a, 0, n);
+	for(int i=0; i<n; i++)
+		cin>>a[i];
+	bubbleSort(a, a+n, comp);
+	for(int i=0; i<n; i++)
+		cout<<a[i]<<" ";
 
 	return 0;
 }
