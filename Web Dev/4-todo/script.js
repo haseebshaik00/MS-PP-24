c92a9c4fd3cc6c7ff9b36d5a9f7c3b26a79bce21
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2024-12-31 22:32:49 -0800

    Working on js

---
+++ Web Dev/4-todo/script.js
@@ -0,0 +1,50 @@
+window.onload = () => {
+    let input = document.getElementById('vanillaInput');
+    let btnAppend = document.getElementById('vanillaAppend');
+    let btnPrepend = document.getElementById('vanillaPrepend');
+    let list = document.getElementById('vanillaList');
+
+    btnAppend.onclick = () => {
+        let up = document.createElement('button');
+        let down = document.createElement('button');
+        let deleteItem = document.createElement('button');
+        up.innerText = '🔼';
+        down.innerText = '🔽';
+        deleteItem.innerText = '❌';
+
+        // 2 options to handle clicks
+        // deleteItem.addEventListener(click, () => {})
+        // deleteItem.onclick = () => {}
+
+        deleteItem.onclick = (event) => {
+            console.log(event);
+            event.target.parentElement.remove();
+        }
+
+        up.onclick = (event) => {
+            const listItem = event.target.parentElement; // Current list item
+            const parent = listItem.parentElement; // Parent list (e.g., <ul> or <ol>)
+            const previousSibling = listItem.previousElementSibling; // Previous list item
+            if (previousSibling)
+                parent.insertBefore(listItem, previousSibling); // Move up
+        }
+
+        down.onclick = (event) => {
+            const listItem = event.target.parentElement; // Current list item
+            const parent = listItem.parentElement; // Parent list (e.g., <ul> or <ol>)
+            const nextSibling = listItem.nextElementSibling; // Next list item
+            if (nextSibling)
+                parent.insertBefore(nextSibling, listItem); // Move down
+        }
+        
+        let listItem = document.createElement('li');
+        let inputNode = document.createElement('span');
+        inputNode.innerText = input.value;
+        listItem.appendChild(deleteItem);
+        listItem.appendChild(up);
+        listItem.appendChild(down);
+        listItem.appendChild(inputNode);
+
+        list.appendChild(listItem);
+    }
+};
