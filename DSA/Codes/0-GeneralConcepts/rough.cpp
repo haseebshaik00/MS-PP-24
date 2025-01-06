de57e4879cc69589d13f97357dd7c8cd345db442
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2025-01-06 03:55:23 -0800

    Done with DSA III

--- DSA/Codes/0-GeneralConcepts/rough.cpp
+++ DSA/Codes/0-GeneralConcepts/rough.cpp
@@ -2,17 +2,6 @@
 #include<iostream>
 using namespace std;
 
-bool comp(int a, int b){
-	return a>b;
-}
-
-void bubbleSort(int a[], int n, bool &cmp(int a, int b)){
-	for(int i=0; i<n-1; i++)
-		for(int j=0; j<n-1-i; j++)
-			if(cmp(a[j], a[j+1]))
-				swap(a[j], a[j+1]);
-}
-
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -23,9 +12,19 @@ int main(){
 	memset(a, 0, n);
 	for(int i=0; i<n; i++)
 		cin>>a[i];
-	bubbleSort(a, a+n, comp);
-	for(int i=0; i<n; i++)
-		cout<<a[i]<<" ";
+	int left=0, right=0, sumMax=INT_MIN;
+	for(int i=0; i<n; i++){
+		for(int j=i; j<n; j++){
+			int s=0;
+			for(int k=i; k<=j; k++)
+				s+=a[k];
+			if(s>=sumMax){
+				left=i; right=j;
+				sumMax = s;
+			}
+		}
+	}
+	cout<<left<<" "<<right<<" "<<sumMax;
 
 	return 0;
 }
