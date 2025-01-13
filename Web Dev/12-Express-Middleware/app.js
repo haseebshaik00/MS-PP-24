6ceeb2fa853281711514910bc6a84cb905f7c329
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2025-01-12 22:46:33 -0800

    Working on Express Routes

--- Web Dev/12-Express-Middleware/app.js
+++ Web Dev/12-Express-Middleware/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json()); // to parse json
-app.use(express.urlencoded({extended : true}));
 app.set('view engine', 'hbs'); // setting view engine as hbs
 app.set('views', __dirname + '/views'); // setting the views directory
+
+app.use(express.json()); // to parse json
+app.use(express.urlencoded({extended : true}));
 app.use("/public", express.static(__dirname + "/public")); // setting the public folder
 // app.use("/todo", todoRoute); // for routing purpose
 
@@ -18,6 +19,8 @@ function m2(req, res, next){
     console.log("Running Middleware M2");
     next();
 }
+
+// check cryptcoder app for more middlewares usage
 
 app.get('/', m1, m2, (req, res) => {
     res.send("Home Page");
