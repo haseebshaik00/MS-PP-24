cf78114c4aba393bae82883e15af12d1c23bb029
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2025-01-29 22:53:13 -0800

    Working on sequelize

--- Web Dev/18-sequelize/query.js
+++ Web Dev/18-sequelize/query.js
@@ -3,10 +3,10 @@ const { db, Professor } = require('./model');
 const task = async () => {
     try{
         await db.sync({alter: true});
-        await Professor.create({
-            firstName: 'Haseeb',
-            age: 24
-        })
+        // sql queries are written in the findAll() function
+        // https://sequelize.org/docs/v6/core-concepts/model-querying-basics/
+        const professors = await Professor.findAll();
+        console.log(professors[0].dataValues);
     }
     catch(err){
         console.log(err);
