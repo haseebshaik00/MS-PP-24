fd3fd2b1f16a43c2f814c01013395311c58eaa7d
haseebshaik00 <haseebshaik00@gmail.com>
2025-06-13 23:53:05 -0700

    Back to Grind

--- DSA/Codes/0-GeneralConcepts/rough.cpp
+++ DSA/Codes/0-GeneralConcepts/rough.cpp
@@ -2,11 +2,18 @@
 #include<iostream>
 using namespace std;
 
+bool comp(int a, int b){
+	return a>b;
+}
+
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	
+	int a[5] {3,4,2,1,5};
+	sort(a, a+5, [](int a, int b) { return a > b; });
+	for(int i=0; i<5; i++)
+		cout<<a[i]<<" ";
 
 	return 0;
 }
