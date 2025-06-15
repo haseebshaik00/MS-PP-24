cf2b678de08f2485482eb584897f8ab3242e5ba5
haseebshaik00 <haseebshaik00@gmail.com>
2025-06-15 04:58:34 -0700

    Done with STL

--- DSA/Codes/0-GeneralConcepts/rough.cpp
+++ DSA/Codes/0-GeneralConcepts/rough.cpp
@@ -6,9 +6,12 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	char x[] ={'2'};
-	string s(x);
-	cout<<s;
+	int a[] = {1, 2, 3};
+    cout << "a       (int*):     " << a << endl; // 0x16bb62a78
+    cout << "&a[0]   (int*):     " << &a[0] << endl; // 0x16bb62a78
+    cout << "&a      (int (*)[3]): " << &a << endl; // 0x16bb62a78
+    cout << "&a + 1  (next array): " << (&a + 1) << endl; // 0x16bb62a84 - similar to whole a + 1
+	cout << "&a + 1  (next array): " << (a + 1) << endl; // 0x16b5c6a7c - similar to &a[0] + 1
 
 	return 0;
 }
