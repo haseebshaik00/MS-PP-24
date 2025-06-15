548a2753b4e3e9d889a2aa2d1d57ceac6a8ed780
haseebshaik00 <haseebshaik00@gmail.com>
2025-06-15 00:48:24 -0700

    Done with V

--- DSA/Codes/0-GeneralConcepts/rough.cpp
+++ DSA/Codes/0-GeneralConcepts/rough.cpp
@@ -2,18 +2,13 @@
 #include<iostream>
 using namespace std;
 
-bool comp(int a, int b){
-	return a>b;
-}
-
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int a[5] {3,4,2,1,5};
-	sort(a, a+5, [](int a, int b) { return a > b; });
-	for(int i=0; i<5; i++)
-		cout<<a[i]<<" ";
+	char x[] ={'2'};
+	string s(x);
+	cout<<s;
 
 	return 0;
 }
