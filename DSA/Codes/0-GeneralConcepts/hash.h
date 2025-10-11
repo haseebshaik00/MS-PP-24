b8aa73c90bc7474a154a6b1fb3e5ad654a1bda25
haseebshaik00 <haseebshaik00@gmail.com>
2025-10-11 10:00:44 -0700

    Added hash DS

--- DSA/Codes/0-GeneralConcepts/hash.h
+++ DSA/Codes/0-GeneralConcepts/hash.h
@@ -1,4 +1,4 @@
-template<typename T>
+template <typename T>
 class Node{
 public:
     string key;
@@ -15,62 +15,127 @@ public:
     }
 };
 
+template <typename T>
 class HashTable{
+
+    int hashFn(string s){
+        int idx=0, p=1;
+        for(int i=0; i<s.length(); ++i){
+            idx += (s[i]*p)%tableSize;;
+            idx = idx%tableSize;
+            p = (p*27)%tableSize;;
+        }
+        return idx;
+    }
+
+    bool isPrime(int n){
+        if(n <= 1) return false;
+        if(n==2 || n==3) return true;
+        if(n%2 == 0 || n%3 == 0) return false;
+        for(int i=5; i*i<=n; i+=6){
+            if(n%i == 0 || n%(i+2)==0) return false;
+        }
+        return true;
+    }
+
+    int nextPrime(int n){
+        int temp = n, flag=false;
+        while(!flag)
+            if(isPrime(++temp)) flag=true;
+        return temp;
+    }
+
+    void rehash(){
+        Node<T> **oldTable = table;
+        int oldTableSize = tableSize;
+        tableSize *= 2; currentSize = 0;
+        tableSize = nextPrime(tableSize);
+        table = new Node<T> *[tableSize];
+        for(int i=0; i<tableSize; i++)
+            table[i] = nullptr;
+        for(int i=0; i<oldTableSize; ++i){
+            Node<T> *temp = oldTable[i];
+            while(temp){
+                insert(temp->key, temp->val);
+                temp = temp->next;
+            }
+            if(oldTable[i]) delete oldTable[i];
+        }
+        delete []oldTable;
+    }
+    
 public:
     Node<T> **table;
     int currentSize, tableSize;
 
     HashTable(int tableSize=7){
+        this->tableSize = tableSize;
         currentSize = 0;
         table = new Node<T> *[tableSize];
         for(int i=0; i<tableSize; ++i)
             table[i] = nullptr;
     }
 
-    int hashFn(string s){
-        int idx=0, p=1;
-        for(int i=0; i<s.length(); ++i){
-            idx += s[i]*p;
-            idx = idx%tableSize;
-            p = p * 27;
+    void insert(string key, T value){
+        int idx = hashFn(key);
+        Node<T> *temp = new Node<T>(key, value);
+        temp->next = table[idx];
+        table[idx] = temp;
+        ++currentSize;
+        // loadfactor & rehash
+        float loadfactor = (currentSize * 1.0)/tableSize;
+        float thresholdValue = 0.75;
+        if(loadfactor >= thresholdValue) rehash();
+    }
+
+    T* search(string key){
+        int idx = hashFn(key);
+        Node<T> *temp = table[idx];
+        while(temp){
+            if(temp->key == key) return &(temp->val);
+            temp = temp->next;
         }
-        return idx;
+        return nullptr;
     }
 
-    bool isPrime(){
-
+    void erase(string key){
+        if(!search(key)) return;
+        int idx = hashFn(key);
+        Node<T> *temp = table[idx], *prev=nullptr;
+        while(temp){
+            if(temp->key == key){
+                if(!prev)   table[idx] = temp->next;
+                else    prev->next = temp->next;
+                temp->next = nullptr;
+                --currentSize;
+                delete temp;
+                return;
+            }
+            prev = temp;
+            temp = temp->next;
+        }
     }
 
-    int nextPrime(){
-
-    }
-
-    void rehash(){
-        Node<T> **oldTable = table;
-
-    }
-
-    insert(){
-
-    }
-
-    erase(){
-
-    }
-
-    search(){
-
-    }
-
-    [](){
-
+    T& operator[](string key){
+        if(!search(key)) insert(key, T{});
+        return *(search(key));
     }
 
     void print(){
-        
+        for(int i=0; i<tableSize; ++i){
+            Node<T> *temp = table[i];
+            cout<<"Bucket= "<<i<<"->";
+            while(temp){
+                cout<<"("<<temp->key<<":"<<temp->val<<")"<<"->";
+                temp = temp->next;
+            }
+            cout<<endl;
+        }
     }
 
     ~HashTable(){
-        
+        for(int i=0; i<tableSize; ++i)
+           if(table[i]) delete table[i];
+        delete []table;
     }
 };
---
+++ DSA/Codes/0-GeneralConcepts/main-hash.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#include "hash.h"
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    HashTable<int> h;
+    h.insert("Burger", 100);
+    h.insert("Idli", 60);
+    h.insert("Dosa", 70);
+    h.insert("Pizza", 80);
+    h.insert("Coke", 40);
+    h.insert("Pepsi", 35);
+    h.insert("Noodles", 75);
+    h.insert("Maggi", 20);
+    h.insert("Samosa", 55);
+    h.print();
+    cout<<endl;
+    int* hSearch = h.search("Maggi");
+    if(hSearch) cout<<"Found = "<<*hSearch<<endl;
+    else cout<<"Not Found!"<<endl;
+    h["Samosa"] = 50;
+    cout<<"Samosa = "<<h["Samosa"]<<endl;
+    h["Maggi"] += 20;
+    cout<<"Maggi = "<<h["Maggi"]<<endl;
+    cout<<endl;
+    h.erase("Noodles");
+    h.print();
+
+    return 0;
+}
